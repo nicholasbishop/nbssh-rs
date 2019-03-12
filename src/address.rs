5454bccbd81fe66b49d2d245b2771c22762f4a4c
Nicholas Bishop <nicholasbishop@gmail.com>
2019-03-11 22:29:27 -0400

    Use to_string for format

--- src/address.rs
+++ src/address.rs
@@ -71,7 +71,7 @@ impl Address {
 
 impl Display for Address {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}", self.host, self.port)
+        write!(f, "{}", self.to_string())
     }
 }
 
